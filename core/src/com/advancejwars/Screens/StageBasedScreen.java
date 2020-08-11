66fac025002cb2498cd6ba8ded094a935a6fc493
cozenom <david.p646@gmail.com>
2020-08-11 11:35:19 -0700

    MVC overhaul in progress

---
+++ core/src/com/advancejwars/Screens/StageBasedScreen.java
@@ -0,0 +1,55 @@
+package com.advancejwars.Screens;
+
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.Screen;
+import com.badlogic.gdx.graphics.GL20;
+import com.badlogic.gdx.scenes.scene2d.Stage;
+import com.badlogic.gdx.utils.viewport.ScreenViewport;
+
+/**
+ * Class to reduce boilerplate when creating a screen that will have a {@link Stage}
+ */
+public abstract class StageBasedScreen implements Screen {
+
+  protected final Stage stage;
+
+  protected StageBasedScreen() {
+    stage = new Stage(new ScreenViewport());
+  }
+
+  @Override
+  public void show() {
+    Gdx.input.setInputProcessor(stage);
+  }
+
+  @Override
+  public void pause() {
+  }
+
+  @Override
+  public void resume() {
+  }
+
+  @Override
+  public void hide() {
+  }
+
+  /*
+  @Override
+  public void dispose() {
+    stage.dispose();
+  }
+*/
+  @Override
+  public void render(float delta) {
+    Gdx.gl.glClearColor(0, 0, 0, 1);
+    Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+
+    stage.act(delta);
+    stage.draw();
+  }
+
+  @Override
+  public void resize(int width, int height) {
+  }
+}
